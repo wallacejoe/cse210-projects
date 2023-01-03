f94897ea7883af5b32e24af730f9c154b84a9448
wallacejoe <JAWcoder@gmail.com>
2023-01-02 23:33:55 -0700

    Program that determines an individuals grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Grade: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Well done, you have successfully passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("You have not passed the class, better luck next time.");
+        }
     }
 }
