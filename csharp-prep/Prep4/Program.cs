f6471fbf96e93e1445c2c5bf007cf3345b384bc4
wallacejoe <JAWcoder@gmail.com>
2023-01-05 16:37:16 -0700

    Numbers list program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        float userInput = 1;
+        List<float> numbers = new List<float>();
+        float sum = 0;
+        float average = 0;
+        float largest = 0;
+        float smallest = 0;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            userInput = int.Parse(Console.ReadLine());
+            numbers.Add(userInput);
+        } while (userInput != 0);
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            sum += numbers[i];
+            if (numbers[i] > largest)
+            {
+                largest = numbers[i];
+            }
+            if (numbers[i] < smallest && numbers[i] > 0)
+            {
+                smallest = numbers[i];
+            }
+        }
+
+        smallest = largest;
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] < smallest && numbers[i] > 0)
+            {
+                smallest = numbers[i];
+            }
+        }
+
+        average = sum / numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+        Console.WriteLine($"The smallest positive number is: {smallest}");
     }
 }
