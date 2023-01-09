c98fe9ff43bdaf77d2551926a259a59dad62e1cd
wallacejoe <JAWcoder@gmail.com>
2023-01-09 13:08:30 -0700

    Completed jobs resume.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Ace Industries";
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Landscaper";
+        job2._company = "Cutting Edge";
+        job2._startYear = 2015;
+        job2._endYear = 2018;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Joseph Wallace";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.DisplayResume();
     }
 }
