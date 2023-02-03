55143c7dc8d04fbe60f49053f51189c3f9d66858
wallacejoe <JAWcoder@gmail.com>
2023-02-03 13:09:57 -0700

    Added more efficient string parser.

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,19 +1,18 @@
 using System;
+using System.Text;
 
 public class Journal
 {
     public List<string> _entries = new List<string>();
-    private String separator = ",";
 
     public void Display()
     {
-
         foreach (string line in _entries)
             {
                 int lineTracker = 1;
-                String[] lineList = line.Split(separator);
-                int lineNum = lineList.Length;
-                foreach (string innerLine in lineList)
+                String[] splitList = SplitCsv(line);
+                int lineNum = splitList.Length;
+                foreach (string innerLine in splitList)
                 {
                     if (lineNum == lineTracker)
                     {
@@ -29,6 +28,35 @@ public class Journal
                     }
                     lineTracker += 1;
                 }
+                Console.WriteLine();
             }
+
+    //Csv string parser, ignores commas inside double quotes.
+    //Found on stackoverflow: https://stackoverflow.com/questions/3776458/split-a-comma-separated-string-with-both-quoted-and-unquoted-strings
+    static string[] SplitCsv(string line)
+    {
+        List<string> result = new List<string>();
+        StringBuilder currentStr = new StringBuilder("");
+        bool inQuotes = false;
+        for (int i = 0; i < line.Length; i++) // For each character
+        {
+            if (line[i] == '\"') // Quotes are closing or opening
+                inQuotes = !inQuotes;
+            else if (line[i] == ',') // Comma
+            {
+                if (!inQuotes) // If not in quotes, end of current string, add it to result
+                {
+                    result.Add(currentStr.ToString());
+                    currentStr.Clear();
+                }
+                else
+                    currentStr.Append(line[i]); // If in quotes, just add it 
+            }
+            else // Add any other character to current string
+                currentStr.Append(line[i]); 
+        }
+        result.Add(currentStr.ToString());
+        return result.ToArray(); // Return array of all strings
+    }
     }
 }
