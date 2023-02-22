63bdff2273792edde7c0e8b74ebaa70bf8ecf9f6
wallacejoe <JAWcoder@gmail.com>
2023-02-22 13:36:16 -0700

    Rectangle class successful test.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -7,5 +7,9 @@ class Program
         Square square = new Square("green", 5);
         Console.WriteLine(square.GetColor());
         Console.WriteLine(square.getArea());
+
+        Rectangle rectangle = new Rectangle("red", 8, 5);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.getArea());
     }
 }
