47f1345406c6ae1df2fa5b990473e82b46042f4b
wallacejoe <JAWcoder@gmail.com>
2023-02-22 13:34:33 -0700

    Complete Rectangle class.

---
+++ prepare/Learning05/Rectangle.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Rectangle : Shape
+{
+    private double _length;
+    private double _width;
+
+    /*Constructor*/
+    public Rectangle(string color, double length, double width) : base(color)
+    {
+        _length = length;
+        _width = width;
+    }
+
+    /*Override Method*/
+    public override double getArea()
+    {
+        double area = _length * _width;
+        return area;
+    }
+}
