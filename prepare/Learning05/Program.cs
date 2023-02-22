ef6c8705859f672fcc5b5f03df749a5f385ccddb
wallacejoe <JAWcoder@gmail.com>
2023-02-22 13:43:30 -0700

    Circle class successful test.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -11,5 +11,9 @@ class Program
         Rectangle rectangle = new Rectangle("red", 8, 5);
         Console.WriteLine(rectangle.GetColor());
         Console.WriteLine(rectangle.getArea());
+
+        Circle circle = new Circle("blue", 10);
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.getArea());
     }
 }
