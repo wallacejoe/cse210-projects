b0bac57dc5aa6445465a32920691401d1289fc44
wallacejoe <JAWcoder@gmail.com>
2023-02-22 13:19:25 -0700

    Complete Shape class.

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,25 @@
+using System;
+
+public abstract class Shape
+{
+    private string _color;
+
+    /*Constructor*/
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    /*Getters/Setters*/
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+    
+    public abstract float getArea();
+}
