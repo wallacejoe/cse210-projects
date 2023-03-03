0b9e15fdaa097cebb4403d7c47b0b3a9276c570c
wallacejoe <JAWcoder@gmail.com>
2023-03-03 14:11:27 -0700

    Added the goal creation menu.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -6,10 +6,10 @@ class Program
     {
         int points = 0;
         string userInput = "";
+        List<Goal> goals = new List<Goal>();
 
         while (userInput != "6")
         {
-            Console.Clear();
             Console.WriteLine($"You have {points} points\n");
         
             Console.WriteLine("Menu Options:");
@@ -24,7 +24,62 @@ class Program
 
             if (userInput == "1")
             {
+                string goalChoice = "";
 
+                Console.Clear();
+                Console.WriteLine("The types of Goals are:");
+                Console.WriteLine(" 1. Simple Goal");
+                Console.WriteLine(" 2. Eternal Goal");
+                Console.WriteLine(" 3. Checklist Goal");
+                Console.Write("Which type of goal would you like to create? ");
+                goalChoice = Console.ReadLine();
+                if (goalChoice == "1")
+                {
+                    try
+                    {
+                        Console.Write("What is the name of your goal? ");
+                        string goalName = Console.ReadLine();
+                        Console.Write("What is a short description of it? ");
+                        string goalDescription = Console.ReadLine();
+                        Console.Write("What is the amount of points associated with this goal? ");
+                        int goalPoints = int.Parse(Console.ReadLine());
+                        goals.Add(new SimpleGoal(goalDescription, goalName, goalPoints));
+                    } catch{Console.WriteLine("Your input did not match the required format");}
+                }
+                else if (goalChoice == "2")
+                {
+                    try
+                    {
+                        Console.Write("What is the name of your goal? ");
+                        string goalName = Console.ReadLine();
+                        Console.Write("What is a short description of it? ");
+                        string goalDescription = Console.ReadLine();
+                        Console.Write("What is the amount of points associated with this goal? ");
+                        int goalPoints = int.Parse(Console.ReadLine());
+                        goals.Add(new EternalGoal(goalDescription, goalName, goalPoints));
+                    } catch{Console.WriteLine("Your input did not match the required format");}
+                }
+                else if (goalChoice == "3")
+                {
+                    try
+                    {
+                        Console.Write("What is the name of your goal? ");
+                        string goalName = Console.ReadLine();
+                        Console.Write("What is a short description of it? ");
+                        string goalDescription = Console.ReadLine();
+                        Console.Write("What is the amount of points associated with this goal? ");
+                        int goalPoints = int.Parse(Console.ReadLine());
+                        Console.Write("How many times does this goal need to be accomplished? ");
+                        int requiredNum = int.Parse(Console.ReadLine());
+                        Console.Write("What is the bonus for accomplishing it that many times? ");
+                        int goalBonus = int.Parse(Console.ReadLine());
+                        goals.Add(new ChecklistGoal(goalBonus, requiredNum, goalDescription, goalName, goalPoints));
+                    } catch{Console.WriteLine("Your input did not match the required format");}
+                }
+                else
+                {
+                    Console.WriteLine("That was not a valid input");
+                }
             }
             else if (userInput == "2")
             {
