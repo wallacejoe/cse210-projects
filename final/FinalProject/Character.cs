c7ebdb587c37202b498ec43f5d7ae8e5fd1d8499
wallacejoe <JAWcoder@gmail.com>
2023-03-20 02:01:41 -0700

    Added basic character creation.

--- final/FinalProject/Character.cs
+++ final/FinalProject/Character.cs
@@ -10,7 +10,38 @@ public class Character
     private List<string> _equipment = new List<string>();
 
     /*Constructors*/
-    public Character(){}
+    public Character()
+    {
+        string userInput = "";
+        _xp = 10;
+        Console.WriteLine("Create your character:");
+        while (_xp > 0)
+        {
+            Console.WriteLine($"Increases left {_xp}");
+            Console.WriteLine("  1. Increase health");
+            Console.WriteLine("  2. Increase mana");
+            Console.WriteLine("  3. Increase stamina");
+            Console.Write("Select a base stat to increase: ");
+            userInput = Console.ReadLine();
+
+            if (userInput == "1")
+            {
+                _health += 1;
+                _xp -= 1;
+            }
+            else if (userInput == "2")
+            {
+                _mana += 2;
+                _xp -= 1;
+            }
+            else if (userInput == "3")
+            {
+                _stamina += 2;
+                _xp -= 1;
+            }
+            else {Console.WriteLine("That was not a valid input");}
+        }
+    }
 
     public Character(int health, int mana, int stamina, int xp, List<string> skills, List<string> spells, List<string> equipment)
     {
@@ -36,6 +67,6 @@ public class Character
 
     public void EquipWeapon()
     {
-        
+
     }
 }
