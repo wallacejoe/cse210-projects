643b8ac22ae815e1dd2df10ef449132de0547b66
wallacejoe <JAWcoder@gmail.com>
2023-03-20 14:41:07 -0700

    Added loot menu loop/error handler.

--- final/FinalProject/Location.cs
+++ final/FinalProject/Location.cs
@@ -47,21 +47,25 @@ public class Location
 
             if (userInput == "2")
             {
-                Console.Clear();
-                int lootNum = 0;
-                int lootInput;
-                foreach (Loot loot in _loot)
-                {
-                    lootNum += 1;
-                    Console.WriteLine($"  {lootNum}. {loot.GetLootContainer()}");
-                }
-                Console.WriteLine($"  {lootNum += 1}. Exit loot menu");
-                Console.Write("Select a choice from the menu: ");
-                lootInput = int.Parse(Console.ReadLine()) - 1;
                 try
                 {
-                    _loot[lootInput].DisplayLoot();
-                    _loot[lootInput].ClaimLoot();
+                    while (_loot.Count() > 0)
+                    {
+                        Console.Clear();
+                        int lootNum = 0;
+                        int lootInput;
+                        foreach (Loot loot in _loot)
+                        {
+                            lootNum += 1;
+                            Console.WriteLine($"  {lootNum}. {loot.GetLootContainer()}");
+                        }
+                        Console.WriteLine($"  {lootNum += 1}. Exit loot menu");
+                        Console.Write("Select a choice from the menu: ");
+                        lootInput = int.Parse(Console.ReadLine()) - 1;
+                        
+                        _loot[lootInput].DisplayLoot();
+                        _loot[lootInput].ClaimLoot();
+                    }
                 } catch {}
             }
             else if (userInput == "3")
