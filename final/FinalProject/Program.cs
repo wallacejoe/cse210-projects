fb47eb77bbbed99816d2e3aef676e061e1913065
wallacejoe <JAWcoder@gmail.com>
2023-04-01 02:10:29 -0700

    Added file load error handling.

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,23 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Would you like to load a saved game (y/n)? ");
-        string userInput = Console.ReadLine();
         Environment environment;
-        
-        if (userInput == "y")
+        bool successfulLoad = false;
+
+        while (!successfulLoad)
         {
-            Console.Write("Enter the file you'd like to load from: ");
-            string filename = Console.ReadLine();
-            File file = new File(filename);
-            file.Deserialize();
-            Character loadCharacter = file.CreateCharacterClass();
-            environment = new Environment(file.GetLocalNum(), loadCharacter, file.CreateLocationClasses());
+            Console.Write("Would you like to load a saved game (y/n)? ");
+            string userInput = Console.ReadLine();
+            
+            successfulLoad = true;
+            if (userInput == "y")
+            {
+                try
+                {
+                    Console.Write("Enter the file you'd like to load from: ");
+                    string filename = Console.ReadLine();
+                    File file = new File(filename);
+                    file.Deserialize();
+                    Character loadCharacter = file.CreateCharacterClass();
+                    environment = new Environment(file.GetLocalNum(), loadCharacter, file.CreateLocationClasses());
+                    environment.MainMenu();
+                } catch {successfulLoad = false;}
+            }
+            else
+            {
+                environment = new Environment();
+                environment.MainMenu();
+            }
         }
-        else
-        {
-            environment = new Environment();
-        }
-        environment.MainMenu();
     }
 }
